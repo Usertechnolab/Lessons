c9eb81ea393949d4981c3ea0c3c15de8b0ec739e
hp15f <hp15f@LAPTOP-1G0PTJV1>
2024-12-17 08:07:52 -0800

    Add project files.

---
+++ Progress/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.CursorVisible=false;
+Console.WriteLine("Task in progress...");
+Console.BackgroundColor=ConsoleColor.White;
+String word = " ";
+int count = 20;
+int delayMilliseconds = 200;
+for (int i = 0; i < count-1; i++)
+{
+    Console.Write(word);
+    await Task.Delay(delayMilliseconds);
+}
+Console.WriteLine(word);
+Console.BackgroundColor = ConsoleColor.Black;
+Console.Write("Finished!");
